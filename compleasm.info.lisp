2d48f6ace147d98baf23da2f8e66fe4c23ae2aa3
Kaiyuan Han <hermit_han@163.com>
2025-08-29 12:58:32 +0800

    update info because we use our own dockerfile to build image

--- compleasm.info.lisp
+++ compleasm.info.lisp
@@ -4,7 +4,9 @@
 (:version        "0.2.7")
 (:intro          "A genome completeness evaluation tool based on miniprot")
 (:dependencies)
-(:functions      "python[3.7.12]"
-                 "compleasm"
-                 "miniprot[0.15-r270]"
-                 "<HMMER>[3.1b2]")
+(:functions      "compleasm"
+                 "python [3.13.2]"
+                 "openjdk [17.0.16 2025-07-15]"
+                 "miniprot [0.15-r270]"
+                 "<HMMER> [3.1b2]"
+                 "<sepp> run_sepp.py [4.5.6]")
