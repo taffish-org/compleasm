569c76d64f3b8a56eb0cc6993c0d59229400ddf7
Kaiyuan Han <hermit_han@163.com>
2025-08-29 11:35:44 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# taf-compleasm
+
+- This is a taf-app(taf-tool), you can use taffish(https://www.taffish.com) to use this taf-app.
+- This app is from https://github.com/schneebergerlab/compleasm
+
+A genome completeness evaluation tool based on miniprot
---
+++ compleasm.info.lisp
@@ -0,0 +1,10 @@
+(:name           "compleasm")
+(:repo           "taffish-org/compleasm")
+(:author         "KaiyuanHan")
+(:version        "0.2.7")
+(:intro          "A genome completeness evaluation tool based on miniprot")
+(:dependencies)
+(:functions      "python[3.7.12]"
+                 "compleasm"
+                 "miniprot[0.15-r270]"
+                 "<HMMER>[3.1b2]")
